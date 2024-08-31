e2c597691fe0e566c8870659682ae22b5930d764
gok-x <drumnbassball@gmail.com>
2024-09-01 01:42:13 +0900

    - add explanations about secret include file

--- src/Secret_Certificates_template.hpp
+++ src/Secret_Certificates_template.hpp
@@ -3,4 +3,8 @@
 const char * const SECRET_CERTIFICATE = R"(-----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
 )";
+const char * const AWS_IOT_ENDPOINT = "your aws iot endpoint url e.g. a3qjEXAMPLEffp-ats.iot.us-west-2.amazonaws.com";
+const int AWS_IOT_ENDPOINT_PORT = "your aws iot port number e.g. 443";
+const char * const AWS_IOT_TOPIC = "your url_encoded_topic_name e.g. device%2Fmessage";
+const char * const AWS_IOT_QOS = "your qos e.g. 1";
 #endif
